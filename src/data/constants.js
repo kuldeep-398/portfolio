4b56191c06cdc47209ed556c1b704c78cd033035
kuldeep-398 <arvindmam398@gmail.com>
2024-08-19 00:10:38 +0530

    done

---
+++ src/data/constants.js
@@ -0,0 +1,366 @@
+export const Bio = {
+  name: "Kuldeep Singh..",
+  roles: [
+    "Front-End Developer",
+    "UI/UX Designer",
+    "ML Engineer",
+    "Power Automate"
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/kuldeep-398",
+  resume:
+    "https://drive.google.com/file/d/1UKIzJww6luz8X7iJ0KkmSHsrxtp1EdLr/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/kuldeep-singh-1298b51ba/",
+  insta: "https://www.instagram.com/kuldeep_singh_8097/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "UI/UX",
+    skills: [
+      {
+        name: "Figma",
+        image: "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg",
+      },
+      {
+        name: "Adobe XD",
+        image: "https://upload.wikimedia.org/wikipedia/commons/c/c2/Adobe_XD_CC_icon.svg",
+      },
+      {
+        name: "Sketch",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/5/59/Sketch_Logo.svg",
+      },
+    ],
+  },
+  {
+    title: "ML Engineer",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/182px-Python-logo-notext.svg.png",
+      },
+      {
+        name: "Supervised & Unsupervised Learning",
+        image:
+          "https://t4.ftcdn.net/jpg/03/62/48/17/360_F_362481762_WYOXzZBC8J63iIu26IeoxXlo7hhIa47w.jpg",
+      },
+      {
+        name: "Numpy",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAATlBMVEVHcExNlc9Nq89Nq89Nq89Nq89Nr89Nq89Nq89Nns9Nps9Nq89Nq89Nq89Nq89Nd89Nq89Nd89Nd89Ndc9Nd89Nd89Nd89Nd89Nd89Nd89FlH+4AAAAGnRSTlMAH11ouOwwrf4KFMZ8TZw81qrNXbn/UHjrjGxZf74AAAEJSURBVHgBrZJFggQxCEWBWPMz5X7/i05Iu+2a8no40G+ERfgbcj4E7z7jmE5FfHwD6pIAksMfa9M6fUCQXGyyKFi7FOwVN9j05jC0IFWp731zddkxXN+3UWPrG8Dl3gFRK+RhnIgNBUuoIWZtUstnOC/Las4tsEUmKmr+Dmdzzy5nx2gizMMdLh1hm7Q6DPIKB3vMDCukwoQ7XDqURxctrCDkRukK930Z+A5VwBFXOA7LPg1nGHwkdjnxDXbFtFoGL0zqHxMa6SiuC4Qwu5dSRuWCakEun+QB6rSPWkwNwrpUYXudC1YlHpeHUnxDj6LbHWqjryt0LSVFehfexlJKEtAn0Q5oQL+Qf6D1EW3rF4ehAAAAAElFTkSuQmCC",
+      },
+      {
+        name: "Pandas",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABDElEQVR4AWPADhqYpBh8uRgY0liRRUXYQ7YLs4f8YWBwYGHAB4Q5g82BCv8LcwS3I4sDxXaBxEEG08YAAYYAAXGGWG5kA1QYPNkFGUL5Qc7GawBIECQJUiTAEaEApKeKsIf6CbOFVoDEhdhD3Ig24P9JBuH/pxhigNiAPAPOMJgDNf8H4nayXQDEUUCsL8wR4gwUmwj0jjpJYSDEHjxTmC044AVbh+5z1vaE1wydUkQbgBwLL1g6Kp6zdPx/ytLuRpYBz9jaNJ4zd4S9ZGgVJ8sAIfZQD2ASni3EFqpFtAFADiMoATEwhDIjxwIokYESG0g1AQMQANkAmBhtDICm9T+gLIsmwQiSg9CYAADqzp3Adb0u+gAAAABJRU5ErkJggg==",
+      },
+      {
+        name: "Seaborn",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAjVBMVEVHcEy6yta4ytLE492+4Nqrvcm84Nqx2dOw29Ou2tKy1Na539ecysmOvsOn08+As759sLx8r7x4qbiQu8aKuMN0o7Vslq5jh6ddf6RcfaKdv8tbeaCJlbFti6xWcZhSaJBtiqt0jq5NXIZLUXxESHZvdJVDRnVeYYhxeZtTV4Fph6lvdZhiZoxjZ41WWoTXqfnjAAAAL3RSTlMACCZIbRSc5///Qq//////////pun//////2T+Sun//+qj//z/Rf+vOuXlaX2d6upTEREAAAE5SURBVHgBZdAFksMwDEBRKczMTpnx/sdbyUFvf7lvZGUCc4iabhi6hgj/M3XLdlzKsS3dVM3zSQZ0XMf3VoSG7a5zbAMXoyFn/H/IndUYT+Qjg5ALIscY99lsDlEUxkmSpmmSxKEt95o+zTAHMf89lqW5SajzjBspxMU0ihZLqBKX5gho0zUkaRJzihYIGs/EaVlxZbryWAOPjiurumnatm2auspmTj3okrJmmFu4g64WrZIQdZnKyQ769jdBp2dp4oEml3GK0/ZSA9y0YrujtttWcbFHwMPueDpzx+NOrPmAAJfT9SqRPk4rrnu+8Tdpg9O8GLcfTKDujxnl/HEryJ53kL1OZ5XPtP0FQ/gmVfn0RhjD1+Oq4ONFNne/0TXPY7c7KJmXz/d8pc7fz8WEn1DrX69eWx34B1WHNIdmGZuvAAAAAElFTkSuQmCC",
+      },
+      {
+        name: "Matplotlib",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Created_with_Matplotlib-logo.svg/2048px-Created_with_Matplotlib-logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "MySQL",
+        image: 
+          "https://upload.wikimedia.org/wikipedia/commons/0/0a/MySQL_textlogo.svg"
+      },
+      {
+        name: "Microsoft Power Automate",
+        image: 
+          "https://play-lh.googleusercontent.com/aeXs0qriXwmHVWtq9u4zVUO6SifULKtJOQdtBg6wDQqaNEaaJKl6b2oiABMmHn6yLH8"
+      },
+      {
+        name: "Automation Anywhere",
+        image: 
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACpElEQVR4AWIgFXwusnD4lG05/1OaFQgbMNATAHqta+DIYSgMwP0cM9+FGZahc1+5r1QfMzMzMzMzM3Oor9xXywyK8vK8UWacaGdXSmGG9xn0S9ENXhJZ6aORBX7KipuTNnKAjZ4AABb5NVZ4KQK+jEzxbW49uslDI6u8V8x9rHgPIiqUA2I73FcAsMarc4ClCNiqtHh8n3N8bKebMkAAd5mA8Qgw1AL2OwkANnuO4C4e8QQRNnWAA84vANjisVkACAKuKCmeOGqviB900thOl+VrRkSgb1IDOGZfCoDdrqXDAK7gW9DlA07YDADscVUMA9AQ8ERq8eTZdhsD0PghpyBsAGEgYrw8wLn2I32AxGEHKQJwBAFEHuBCewAARxzjiwBUIKBHTvGrrToD0MRJm7h5qYjm1PWWKwA43S7+s2VHc/pOw/jUjRaavNAmbtsqojl9t4EA4FLbEfHZCqI5fa/+CwAutwlvIj2aM09qK9L362nqRrPwb1YSzZmntUsBcKtpaamAsqI587zGQECFxZiwIrLCR3BQ+gWL8JOB+7+UFM3ZN3NtDEDTD+qfDO6OYTzwBUZEOCjlC3JTwAJV/Og593bOEQA8rCMQRufbNbMzwhFRDwNsZQBN0BR1rjWY0xNhQOXezw5knlcH2CeoYEH0BIIIOyPWHVsXFUdzgE0Gt279b+W/TNUZgGZeVPewZtjTH0TtRuKUreRxP/cv2DAlA0M20fy3qVcAwD4B/IQ3m6+wHCina+UzAQINIT3cPiz+Z9z4/PcptAB4UM8LywGM5zOB2wefZADwdxwpAF5UWxaXkAnE4tNU9AP+jfkCgA+zvsAOuQCNa7J6ocfkkzL/fzTNf58cyH2awX1zJW+BnwgHGHMl/2OyMDYl/JBbcSq0rF4lYWfc+lvcAwAAAABJRU5ErkJggg=="
+      },
+      {
+        name: "UiPath Studio",
+        image: 
+          "https://media.licdn.com/dms/image/D4E12AQFOTZtNkdF6WQ/article-cover_image-shrink_720_1280/0/1702400067845?e=2147483647&v=beta&t=JTcepqf4AwTmiEhLu1XzaHt40UoLmqeoxGiJErYFdoU"
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://cio.eletsonline.com/wp-content/uploads/2021/06/upgrad.jpg",
+    role: "Machine Learning Intern",
+    company: "UpGrad",
+    date: "15th Jan 2024 - 3rd May 2024",
+    desc: "Worked on Healthcare Insurance Project, managing the insurance cost, and streamlining the process with Machine Learning and Regression Algorithms.",
+    skills: [
+      "Python",
+      "Streamlit",
+      "Data Analysis",
+      "Machine Learning Algorithms",
+      "Git/Github",
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Django",
+      "AWS",
+    ],
+    doc: "https://media.licdn.com/dms/image/v2/D562DAQH2DZ4VtMWr4w/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1724003718101?e=1724608800&v=beta&t=1Xn1mUNKoAerHY6P1NvssFhgP2d3J_zNrHJR3Ge5eZ4",
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/3/3a/Lovely_Professional_University_logo.png",
+    school: "Lovely Professional University, Phagwara (Punjab)",
+    date: "Oct 2020 - May 2024",
+    grade: "8.04 CGPA",
+    desc: "I recently graduated with a Bachelor’s degree in Computer Science and Engineering from Lovely Professional University in Phagwara. I achieved a CGPA of 8.04 during my studies. My coursework covered a range of topics, including Machine Learning, Deep Learning, Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://www.gmsss32.in/wp-content/uploads/2022/09/cropped-logochd1-1.png",
+    school: "GMSSS-32C, Chandigarh",
+    date: "Apr 2018 - Apr 2019",
+    grade: "79.8%",
+    desc: "I completed my class 12 high school education at GMSSS-32C, Chandigarh, where I studied Science (Math).",
+    degree: "CBSE, Science",
+  },
+  {
+    id: 2,
+    img: "https://play-lh.googleusercontent.com/YZeYGkCDOymjC6J3EfLuWTWwXZZctU6cEjC6zPCiAw61z4FhQWNg5NMYK70zw7Erzg",
+    school: "Air Force School, 3BRD Chandigarh",
+    date: "Apr 2016 - Apr 2017",
+    grade: "9.8 CGPA",
+    desc: "I completed my class 10 education at Air Force School, 3BRD Chandigarh",
+    degree: "CBSE",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Healthcare Insurance Cost",
+    date: "Jan 2024 - May 2024",
+    description:
+      "Medical insurance is one of the most crucial aspects of healthcare, and machine learning techniques can play a significant role in analyzing and predicting medical insurance costs. The goal of this project is to build a predictive model that can estimate the medical insurance costs for individuals based on their age, gender, BMI, smoking status, and other related factors. The dataset used in this project contains information about medical insurance costs for a group of people based on these factors. ",
+    image:
+      "https://media.licdn.com/dms/image/D5622AQEIyn6Ho67aIw/feedshare-shrink_2048_1536/0/1721124257211?e=1724284800&v=beta&t=Qp49Temj5VnWj7AlFTIgt0vdYGwC6te6Fe6wwCQwQrk",
+    tags: [
+      "Machine Learning",
+      "Algorithms",
+      "NumPy",
+      "Pandas",
+      "Matplotlib",
+      "Seaborn",
+      "Streamlit",
+      "Github",
+      "Python",
+    ],
+    category: "machine learning",
+    github: "https://github.com/Mrdeep3c/Healthcare_Prediction",
+    webapp: "https://healthcareprediction-ju2x73mvjf85mgm6aba9kr.streamlit.app/?utm_medium=social",
+  },
+  {
+    id: 0,
+    title: "Image Recognization CIFAR-10",
+    date: "Mar 2023 - May 2023",
+    description:
+      "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d68ad885bf435e.png",
+    tags: ["Machine Learning",
+      "Algorithms",
+      "NumPy",
+      "Pandas",
+      "Matplotlib",
+      "Seaborn",
+      "Github",
+      "Python",],
+    category: "machine learning",
+    github: "https://github.com/Mrdeep3c/Cifar-10",
+    webapp: "",
+  },
+  {
+    id: 1,
+    title: "RBK Education Institutions Clone",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "Designed and developed the RBK Education Institutions School website clone.",
+    image:
+      "https://media.licdn.com/dms/image/D5622AQG7m_uanoq4AQ/feedshare-shrink_2048_1536/0/1721125846637?e=1724284800&v=beta&t=DAEk6wytJw4UVZoPKV3yWS8U27JWoCspqALbcm6CiPo",
+    tags: [
+      "HTML",
+      "CSS",
+      "Js",
+      "Tailwind",
+      "Github",
+    ],
+    category: "web app",
+    github: "https://github.com/Mrdeep3c/School-Website",
+    webapp: "https://mrdeep3c.github.io/School-Website/",
+  },
+  {
+    id: 2,
+    title: "Drum-Kit",
+    date: "Jul 2023 - Aug 2023",
+    description:
+      "Drum Kit Website, the ultimate online and offline drum kit experience! Whether you're a seasoned drummer or just starting out, our interactive drum kit allows you to practice and play right from your browser using mouse or keyboard.",
+    image:
+      "https://cdn.dribbble.com/userupload/15624878/file/original-d37e0da0311979e8b24b62ff96b5367a.png?resize=2140x1174",
+    tags: ["HTML", "CSS", "JS", "Github"],
+    category: "web app",
+    github: "https://github.com/Mrdeep3c/Drum-Kits",
+    webapp: "https://mrdeep3c.github.io/Drum-Kits/",
+  },
+  {
+    id: 3,
+    title: "Dice Game",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "The webpage has been developed using HTML, CSS, and JavaScript to create a dice game for two players, with functionality to determine the winner by simply refreshing the page.",
+    image:
+      "https://cdn.dribbble.com/userupload/15625086/file/original-6d2a7a4e5b61a0d616253e69b2fa4112.png?resize=2158x1177",
+    tags: ["HTML", "CSS", "JS", "Github"],
+    category: "web app",
+    github: "https://github.com/Mrdeep3c/Dice-Game",
+    webapp: "https://mrdeep3c.github.io/Dice-Game/",
+  },
+  {
+    id: 10,
+    title: "Tindog",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Tindog is a clone of the Tinder website, created using HTML, CSS, and Bootstrap. This project showcases my ability to replicate complex web applications, featuring a responsive design and user-friendly interface. It demonstrates my skills in front-end development and my proficiency with modern web development technologies.",
+    image:
+      "https://cdn.dribbble.com/userupload/15625299/file/original-1315fd6b45801c214c745a6e086bf2c9.png?resize=1435x955",
+    tags: ["HTML", "CSS", "JS", "Bootstrap", "Tailwind", "Github"],
+    category: "web app",
+    github: "https://github.com/Mrdeep3c/Tindog",
+    webapp: "https://mrdeep3c.github.io/Tindog/",
+  },
+  {
+    id: 4,
+    title: "Move it",
+    date: "May 2023 - Jun 2023",
+    description:
+      "Move It is a website developed using HTML, CSS, and Bootstrap, dedicated to providing exceptional moving services to customers across the US. As a startup, our mission is to simplify and enhance the moving experience, ensuring it is stress-free and straightforward. We are passionate about making the process as seamless and enjoyable as possible, reflecting our commitment to customer satisfaction and innovation in the moving industry.",
+    image:
+      "https://cdn.dribbble.com/userupload/15625433/file/original-b1be6dda4aed1849300e5d8aa5955daa.png?resize=2158x1173",
+    tags: ["HTML", "CSS", "JS", "Bootstrap", "Tailwind", "Github"],
+    category: "web app",
+    github: "https://github.com/Mrdeep3c/Move-It",
+    webapp: "https://mrdeep3c.github.io/Move-It/",
+  },
+  {
+    id: 5,
+    title: "Nike Air Max",
+    date: "Jun 2023",
+    description:
+      "I designed a user interface for the NIKE AIR MAX 90 shoe using Figma, incorporating creative ideas and providing comprehensive instructions and details. This project highlights my skills in UI/UX design, showcasing my ability to blend aesthetics with functionality to create an engaging and informative user experience.",
+    image:
+      "https://media.licdn.com/dms/image/C4D22AQFXgRLH7FcI5A/feedshare-shrink_2048_1536/0/1674407182835?e=1724284800&v=beta&t=6jaagfPmCGl7PjDbDjd763EJnm376wpY8sobKc0RjVI",
+    tags: ["UI", "UX", "Figma", "Photoshop"],
+    category: "UI/UX",
+    github: "https://github.com/Mrdeep3c/NIKE-AIRMAX-90",
+    webapp: "https://www.figma.com/design/2ziOweCPq2bfA5ZcvX7Twq/NIKE-AIR-MAX-90?node-id=1-2&t=dBhmPraRSIO7UZey-1",
+  },
+  {
+    id: 6,
+    title: "Gaming Console",
+    date: "Jun 2023",
+    description:
+      "I designed a user interface for the Gaming Console using Figma, incorporating creative ideas and providing comprehensive instructions and details. This project highlights my skills in UI/UX design, showcasing my ability to blend aesthetics with functionality to create an engaging and informative user experience.",
+    image:
+      "https://cdn.dribbble.com/userupload/15625641/file/original-8814a347096b3d6fbaf4f88414edfd12.png?resize=752x",
+    tags: ["UI", "UX", "Figma", "Photoshop"],
+    category: "UI/UX",
+    github: "https://github.com/Mrdeep3c/Gaming-Console-UI-Design-",
+    webapp: "https://www.figma.com/design/BEmSdyP8dUK1vz4PHYpBGv/Console?node-id=0-1&t=9BHYQDscWs6quhE0-1",
+  },
+  {
+    id: 7,
+    title: "SignIn/SignUp Page",
+    date: "May 2023",
+    description:
+      "I designed a user interface for the SignIn/SignUp Page using Figma, incorporating creative ideas and providing comprehensive instructions and details. This project highlights my skills in UI/UX design, showcasing my ability to blend aesthetics with functionality to create an engaging and informative user experience.",
+    image:
+      "https://cdn.dribbble.com/userupload/8541773/file/original-b4c2dd5da53ce371f51a370d7274407f.jpg?resize=1504x1039",
+    tags: ["UI", "UX", "Figma", "Prototype", "Photoshop"],
+    category: "UI/UX",
+    github: "",
+    webapp: "https://www.figma.com/design/dADRdxATcvBBOCafVZCUEN/Log-In?node-id=0-1&t=0AjnVPcIBbf8mEed-1",
+  },
+  {
+    id: 8,
+    title: "FinStreet Treding Website",
+    date: "Jan 2021",
+    description:
+      "I designed a user interface for the FinStreet into modern style clone using Figma, incorporating creative ideas and providing comprehensive instructions and details. This project highlights my skills in UI/UX design, showcasing my ability to blend aesthetics with functionality to create an engaging and informative user experience.",
+    image:
+      "https://cdn.dribbble.com/userupload/15625830/file/original-bed51853e57b1417d2925d2d0426dff5.png?resize=1504x862",
+    tags: ["UI", "UX", "Figma", "Prototype", "Photoshop"],
+    category: "UI/UX",
+    github: "https://www.figma.com/design/pVEgVK8IvOqVlD5ZojEimx/FinStreet?node-id=0-1&t=3LuwrQQU66a3kzWK-1",
+    webapp: "https://www.figma.com/proto/pVEgVK8IvOqVlD5ZojEimx/FinStreet?page-id=0%3A1&node-id=2-2&viewport=427%2C24%2C0.12&t=l2gsZKUXzbdk26cf-1&scaling=min-zoom&content-scaling=fixed",
+  },
+];
+
+
